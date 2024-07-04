f359f9631f044cffc52267c83558769cac4ee13e
acerohernan <contacto.acero.hernan@gmail.com>
2024-07-04 02:31:00 -0500

    feat: Componente 'Toaster' añadido para renderizar los toasts

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,15 @@
 import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 
+import { Toaster } from "./components/ui/toaster";
+
 import { router } from "./router";
 
 function App() {
 	return (
 		<Suspense>
 			<RouterProvider router={router} />
+			<Toaster />
 		</Suspense>
 	);
 }
