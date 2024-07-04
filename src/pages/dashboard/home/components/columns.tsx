8b3acff984f7fc9d5cb2b7faa455255cd2610f1c
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-07-03 22:59:50 -0500

    fix: columna de prioridades eliminada

--- src/pages/dashboard/home/components/columns.tsx
+++ src/pages/dashboard/home/components/columns.tsx
@@ -3,7 +3,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Badge } from "@/components/ui/badge";
 import { Checkbox } from "@/components/ui/checkbox";
 
-import { labels, priorities, statuses } from "../data/data";
+import { labels, statuses } from "../data/data";
 import { Task } from "../data/schema";
 import { DataTableColumnHeader } from "./data-table-column-header";
 import { DataTableRowActions } from "./data-table-row-actions";
@@ -84,33 +84,6 @@ export const columns: ColumnDef<Task>[] = [
 						<status.icon className="mr-2 h-4 w-4 text-muted-foreground" />
 					)}
 					<span>{status.label}</span>
-				</div>
-			);
-		},
-		filterFn: (row, id, value) => {
-			return value.includes(row.getValue(id));
-		},
-	},
-	{
-		accessorKey: "priority",
-		header: ({ column }) => (
-			<DataTableColumnHeader column={column} title="Priority" />
-		),
-		cell: ({ row }) => {
-			const priority = priorities.find(
-				(priority) => priority.value === row.getValue("priority")
-			);
-
-			if (!priority) {
-				return null;
-			}
-
-			return (
-				<div className="flex items-center">
-					{priority.icon && (
-						<priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-					)}
-					<span>{priority.label}</span>
 				</div>
 			);
 		},
