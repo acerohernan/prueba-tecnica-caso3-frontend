36929fa3f760bd09ea74108e53bce9637e38ae4b
acerohernan <contacto.acero.hernan@gmail.com>
2024-07-04 05:49:40 -0500

    conf: configuración de proveedor de react-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,24 @@
 import { Suspense } from "react";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Toaster } from "./components/ui/toaster";
 
 import { router } from "./router";
+import { store } from "./store";
+
+const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<Suspense>
-			<RouterProvider router={router} />
-			<Toaster />
+			<QueryClientProvider client={queryClient}>
+				<Provider store={store}>
+					<RouterProvider router={router} />
+					<Toaster />
+				</Provider>
+			</QueryClientProvider>
 		</Suspense>
 	);
 }
