49aac86a0420c4772ca5a2391c7cb3a302ddccb8
acerohernan <contacto.acero.hernan@gmail.com>
2024-07-04 06:47:00 -0500

    feat: integración con el API para el modal de eliminar usuario

--- src/components/dialogs/delete-user-dialog/index.tsx
+++ src/components/dialogs/delete-user-dialog/index.tsx
@@ -1,4 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+
+import { API } from "@/api";
+
 import {
 	Dialog,
 	DialogContent,
@@ -7,8 +11,10 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
-import { useAppDispatch } from "@/store";
+import { useAppDispatch, useAppSelector } from "@/store";
 import { navigationActions } from "@/store/navigation";
 
 interface IProps {
@@ -16,13 +22,46 @@ interface IProps {
 }
 
 export function DeleteUserDialog({ open }: IProps) {
+	const [loading, setLoading] = useState(false);
 	const dispatch = useAppDispatch();
+	const selectedUser = useAppSelector((state) => state.navigation.selectedUser);
+	const queryClient = useQueryClient();
+	const { toast } = useToast();
+
+	function closeModal() {
+		dispatch(navigationActions.closeAllModals());
+	}
+
+	async function handleDeleteUser() {
+		if (!selectedUser) {
+			closeModal();
+			return;
+		}
+
+		setLoading(true);
+		const res = await API.users.deleteUser(selectedUser.id);
+		setLoading(false);
+
+		if (res.success) {
+			await queryClient.invalidateQueries({ queryKey: ["users"] });
+			closeModal();
+			return;
+		}
+
+		toast({
+			variant: "destructive",
+			title: "Error al eliminar usuario",
+			description:
+				"Oh! lo sentimos. Algo salió mal y no podremos eliminar al usuario",
+		});
+		closeModal();
+	}
 
 	return (
 		<Dialog
 			open={open}
 			onOpenChange={(open) => {
-				if (!open) dispatch(navigationActions.closeAllModals());
+				if (!open) closeModal();
 			}}
 		>
 			<DialogContent className="sm:max-w-[500px]">
@@ -36,11 +75,17 @@ export function DeleteUserDialog({ open }: IProps) {
 					<Button
 						type="button"
 						variant="outline"
-						onClick={() => dispatch(navigationActions.closeAllModals())}
+						onClick={closeModal}
+						disabled={loading}
 					>
 						Cancelar
 					</Button>
-					<Button type="button" variant="destructive">
+					<Button
+						type="button"
+						variant="destructive"
+						onClick={handleDeleteUser}
+						disabled={loading}
+					>
 						Eliminar
 					</Button>
 				</DialogFooter>
