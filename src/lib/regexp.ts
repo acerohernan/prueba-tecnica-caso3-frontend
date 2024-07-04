0935f5964c8fd93d4df248529f2ad02c505db59e
acerohernan <contacto.acero.hernan@gmail.com>
2024-07-04 05:49:17 -0500

    feat: expresión regular para validar contraseña agregada

--- src/lib/regexp.ts
+++ src/lib/regexp.ts
@@ -1,3 +1,6 @@
 /* eslint-disable no-useless-escape */
 
 export const emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+export const atLeastOneNumberUppercaseSpecialRegExp =
+	/^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).*$/;
